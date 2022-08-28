a2621f75282975c4d3cf785b2238e2e466107074
Nick <wagemannnick@gmail.com>
2022-08-28 13:53:59 -0400

    created unit test for account, post, payment

---
+++ fastapi-accounts/tests/test_accounts.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+from main import app
+from database import fetch_all_account
+
+client = TestClient(app)
+
+class EmptyAccountQueries:
+    def fetch_all_account(self):
+        return []
+
+def test_get_all_accounts():
+    app.dependency_overrides[fetch_all_account] = EmptyAccountQueries
+
+    response = client.get("/api/post")
+
+    assert response.status_code == 200
+    assert response.json() == {"account": []}
