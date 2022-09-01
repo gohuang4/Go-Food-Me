ebbc2b2de728498a73c348be5b271ec10a20ebc7
Nick <wagemannnick@gmail.com>
2022-09-01 19:25:47 -0400

    client fix

--- payments/database.py
+++ payments/database.py
@@ -1,11 +1,17 @@
+from http import client
+import os
+import requests
 import motor.motor_asyncio
 from bson.objectid import ObjectId
 from model import PaymentGetAll
 
 
-client = motor.motor_asyncio.AsyncIOMotorClient(
-    "mongodb://root:password@mongo"
-    )
+# client = motor.motor_asyncio.AsyncIOMotorClient(
+#     "mongodb://root:password@mongo"
+#     )
+url = f"{os.environ['DATABASE_URL']}"
+client = motor.motor_asyncio.AsyncIOMotor(url)
+response = requests.get(url)
 database = client.PaymentList
 collection = database.payment
 
