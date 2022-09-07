133b64c76748b439ae2d68502df17d129f0982a8
Jeremiah Dyck <jeremiahdyck01@gmail.com>
2022-09-07 11:35:47 -0600

    11:35

--- ghi/src/SignupForm.jsx
+++ ghi/src/SignupForm.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react';
+import UseForm from "./UseForm.js";
 
 function BootstrapInput(props) {
   const { id, placeholder, labelText, value, onChange, type } = props
@@ -11,6 +12,8 @@ function BootstrapInput(props) {
   )
 }
 
+// old
+/*
 function SignupForm(props) {
   const [name, setName] = useState('')
   const [password, setPassword] = useState('')
@@ -46,3 +49,79 @@ function SignupForm(props) {
     )
 }
 export default SignupForm
+*/
+
+const FORM_ENDPOINT = "https://herotofu.com/start";
+
+const SignupForm = () => {
+  const formElement = useRef(null);
+  const additionalData = {
+    sent: new Date().toISOString(),
+  };
+
+  const { handleSubmit, status, message } = UseForm({
+    form: formElement.current, 
+    additionalData,
+  });
+
+  if (status === "success") {
+    return (
+      <>
+        <div>Thank you!</div>
+        <div>{message}</div>
+      </>
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <>
+        <div>Something bad happened!</div>
+        <div>{message}</div>
+      </>
+    );
+  }
+
+  return (
+    <form
+      action={FORM_ENDPOINT}
+      onSubmit={handleSubmit}
+      method="POST"
+      target="_blank"
+      ref={formElement}
+    >
+      <div className="mb-3 pt-0">
+        <input
+          type="text"
+          placeholder="Your name"
+          name="name"
+          required
+        />
+      </div>
+      <div className="mb-3 pt-0">
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          required
+        />
+      </div>
+      <div className="mb-3 pt-0">
+        <textarea
+          placeholder="Your message"
+          name="message"
+          required
+        />
+      </div>
+      {status !== "loading" && (
+        <div className="mb-3 pt-0">
+          <button type="submit">
+            Send a message
+          </button>
+        </div>
+      )}
+    </form>
+  );
+};
+
+export default SignupForm;
