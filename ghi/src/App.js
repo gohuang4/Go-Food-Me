34e9ac6d3f07dea550ef460da428af825bf13290
Nick <wagemannnick@gmail.com>
2022-09-06 22:08:01 -0400

    console error clear react pages

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Nav from './Nav';
+import Nav from './Nav'
 import MainPage from './MainPage';
 import HowItWorks from './HowItWorks'
 import PostForm from './PostForm';
 
 function App(props) {
+  const domain = /https:\/\/[^/]+/;
   const basename = process.env.PUBLIC_URL;
   return (
     <BrowserRouter basename={basename}>
