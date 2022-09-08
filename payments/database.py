cee1d9b3bb38563ea4d6eb3e4ec4ad9276907a89
Nick <wagemannnick@gmail.com>
2022-09-07 23:04:30 -0400

    test

--- payments/database.py
+++ payments/database.py
@@ -16,6 +16,7 @@ def move_ids_around(doc):
     del document["_id"]
     return document
 
+
 async def fetch_all_payments():
     payments = []
     cursor = collection.find({})
@@ -24,10 +25,12 @@ async def fetch_all_payments():
         payments.append(PaymentGetAll(**doc))
     return payments
 
+
 async def fetch_one_payment(id):
     o_id = ObjectId(id)
     document = await collection.find_one({"_id": o_id})
     return document
+
 
 async def create_payment(Payment):
     document = Payment
@@ -35,7 +38,14 @@ async def create_payment(Payment):
     return document
 
 
-async def update_payment(id, name, card_number, expiration_date, CVV, donation_date):
+async def update_payment(
+    id,
+    name,
+    card_number,
+    expiration_date,
+    CVV,
+    donation_date
+):
     o_id = ObjectId(id)
     await collection.update_one(
         {"_id": o_id},
