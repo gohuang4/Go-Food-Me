ef2e93067e0dd2d874ca732091209bdb965459c2
Jeremiah Dyck <jeremiahdyck01@gmail.com>
2022-09-15 15:26:01 -0600

    unit test

--- accounts/tests/test_accounts.py
+++ accounts/tests/test_accounts.py
@@ -8,3 +8,9 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Go": "FoodMe"}
+
+
+def test_bad_id():
+    response = client.get("/api/account/123")
+    assert response.status_code == 404
+    assert response.json() == ["There is no account with this id.123"]
