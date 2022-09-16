eb4bd3eee11df1ad74e8e9cd99a457e6bdaa47c8
Nick <wagemannnick@gmail.com>
2022-09-16 18:26:47 -0400

    test

--- posts/tests/test_posts.py
+++ posts/tests/test_posts.py
@@ -2,8 +2,8 @@ from fastapi.testclient import TestClient
 from main import app
 import json
 # from database import fetch_all_post
-from httpx import AsyncClient
-client = TestClient(app)
+
+# client = TestClient(app)
 
 
 def test_read_main():
@@ -11,7 +11,46 @@ def test_read_main():
     assert response.status_code == 200
     assert response.json() == {"Go": "FoodMe"}
 
+@pytest.fixture(scope="function")
+def app() => Generator[FastAPI, Any, None]:
+    """
+    Create a fresh database on each test case.
+    """
+    Base.metadata.create_all(engine)  # Create the tables.
+    _app = start_application()
+    yield _app
+    Base.metadata.drop_all(engine)
 
+
+@pytest.fixture(scope="function")
+def db_session(app: FastAPI) => Generator[SessionTesting, Any, None]:
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = SessionTesting(bind=connection)
+    yield session  # use the session in tests.
+    session.close()
+    transaction.rollback()
+    connection.close()
+
+
+@pytest.fixture(scope="function")
+def client(
+    app: FastAPI, db_session: SessionTesting
+) -> Generator[TestClient, Any, None]:
+    """
+    Create a new FastAPI TestClient that uses the `db_session` fixture to override
+    the `get_db` dependency that is injected into routes.
+    """
+
+    def _get_test_db():
+        try:
+            yield db_session
+        finally:
+            pass
+
+    app.dependency_overrides[get_db] = _get_test_db
+    with TestClient(app) as client:
+        yield client
 
 def test_create_user(client):
     data = {"id":"6323678424e973e93b226bbd","title": "test","picture_url": "test","description": "test", "requested_amount": 100}
