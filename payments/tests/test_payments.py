de6a5c577f2e3a1192ea1029a3e3d6a196017d47
Nick <wagemannnick@gmail.com>
2022-09-16 19:05:21 -0400

    test

--- payments/tests/test_payments.py
+++ payments/tests/test_payments.py
@@ -8,3 +8,15 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Go": "FoodMe"}
+
+async def override_dependency(id:str):
+    return "Sucessfully deleted payment"
+
+app.dependency_overrides[delete_payment] = override_dependency
+
+def test_delete_post():
+    response = client.delete("/api/payments/1")
+    assert response.status_code == 200
+    assert response == "Sucessfully deleted payment"
+
+app.dependency_overrides = {}
