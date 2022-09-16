d59e584f67fc636a764b72603bd388149c6004c0
Gordon <gohuang4@gmail.com>
2022-09-15 18:03:55 -0700

    sadge

--- posts/tests/test_posts.py
+++ posts/tests/test_posts.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 from main import app
-from database import fetch_all_post
+# from database import fetch_all_post
 
 client = TestClient(app)
 
@@ -40,10 +40,10 @@ def test_invalid_post_id1():
 class EmptyPostQueries:
     def fetch_all_post(self):
         return []
-        
+
 def test_get_all():
     # fake_db = []
-    app.dependency_overrides[fetch_all_post] = EmptyPostQueries
+    # app.dependency_overrides[fetch_all_post] = EmptyPostQueries
     response = client.get("/api/post")
     assert response.status_code == 200
     assert response.json() == {'post': []}
