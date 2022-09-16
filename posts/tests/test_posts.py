6d7fc3bdde4e1f047f76b24204901e41f4d13a19
Nick <wagemannnick@gmail.com>
2022-09-16 18:08:18 -0400

    test

--- posts/tests/test_posts.py
+++ posts/tests/test_posts.py
@@ -1,12 +1,13 @@
-from fastapi.testclient import TestClient
+# from fastapi.testclient import TestClient
 from main import app
-from database import fetch_all_post
+# from database import fetch_all_post
+from httpx import AsyncClient
+# client = TestClient(app)
 
-client = TestClient(app)
-
-
+@pytest.mark.anyio
 def test_read_main():
-    response = client.get("/")
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/")
     assert response.status_code == 200
     assert response.json() == {"Go": "FoodMe"}
 
@@ -25,15 +26,15 @@ def test_read_main():
 #     assert response.json() == {'detail': 'Not Found'}
 
 
-class EmptyPostQueries:
-    post = []
+# class EmptyPostQueries:
+#     post = []
 
 
-def test_get_all():
-    app.dependency_overrides(fetch_all_post: EmptyPostQueries)
-    response = client.get("/api/post")
-    assert response.status_code == 200
-    assert response.json() == {"post": []}
+# def test_get_all():
+#     app.dependency_overrides(fetch_all_post: EmptyPostQueries)
+#     response = client.get("/api/post")
+#     assert response.status_code == 200
+#     assert response.json() == {"post": []}
 
 
 
