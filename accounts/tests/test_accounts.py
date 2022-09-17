fc393470da576a6a58c1dea6472d752979e3bd4c
Nick <wagemannnick@gmail.com>
2022-09-16 20:57:10 -0400

    lint

--- accounts/tests/test_accounts.py
+++ accounts/tests/test_accounts.py
@@ -3,14 +3,8 @@ from main import app
 
 client = TestClient(app)
 
-
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 401
     assert response.json() == {'detail': 'Invalid authentication credentials'}
 
-
-def test_bad_id():
-    response = client.get("/api/account/123")
-    assert response.status_code == 405
-    assert response.json() == {"detail": "Method Not Allowed"}
--- payments/tests/test_payments.py
+++ payments/tests/test_payments.py
@@ -1,23 +1,9 @@
 from fastapi.testclient import TestClient
-from main import app, delete_payment
+from main import app
 
-
+client = TestClient(app)
 
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Go": "FoodMe"}
-
-async def override_dependency(id:str):
-    return "Sucessfully mock data delete"
-
-app.dependency_overrides[delete_payment] = override_dependency
-
-client = TestClient(app)
-
-def test_delete_post():
-    response = client.delete("/api/payment/6324c248a246d6711bba0f28")
-    assert response.status_code == 200
-    assert response.content == b'"Sucessfully mock data delete"'
-
-app.dependency_overrides = {}
