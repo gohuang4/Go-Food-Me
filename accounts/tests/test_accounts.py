8e4716b18b5b780848dc6e0d818fb686e5f9f57b
Nick <wagemannnick@gmail.com>
2022-09-16 21:19:01 -0400

    lint

--- accounts/tests/test_accounts.py
+++ accounts/tests/test_accounts.py
@@ -3,8 +3,8 @@ from main import app
 
 client = TestClient(app)
 
+
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 401
-    assert response.json() == {'detail': 'Invalid authentication credentials'}
-
+    assert response.json() == {"detail": "Invalid authentication credentials"}
--- accounts/tests/test_posts.py
+++ accounts/tests/test_posts.py
@@ -2,4 +2,3 @@ from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
-
